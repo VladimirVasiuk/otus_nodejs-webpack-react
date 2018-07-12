59398c8b652ae6b00db3ece810c95fe53e378ae5
ydvorzhetskiy <dvorzhetskij@mail.ru>
2018-07-12 22:30:05 +0600

    5 - props

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Header = () => (
+const Header = (props) => (
     <div>
-        <h1>ШАПКА САЙТА</h1>
+        <h1>{props.title}</h1>
     </div>
 );
 
 const App = () => {
   return <div>
-    <Header/>
+    <Header title="ШАПКА САЙТА"/>
     <h3>Hello World!</h3>
   </div>
 };
