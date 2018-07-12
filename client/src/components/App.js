0e6631df8688834b7c12bcf12973b597a0030653
ydvorzhetskiy <dvorzhetskij@mail.ru>
2018-07-12 22:28:56 +0600

    4 - header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+const Header = () => (
+    <div>
+        <h1>ШАПКА САЙТА</h1>
+    </div>
+);
+
 const App = () => {
-  return <h3>Hello World!</h3>
-}
+  return <div>
+    <Header/>
+    <h3>Hello World!</h3>
+  </div>
+};
 
 export default App
+
+
